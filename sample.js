5be39789d11e0175f05c968db6f5c0f6d107979c
NIKU-SINGH <soapmactavishmw4@gmail.com>
2023-10-12 19:17:24 +0530

    new js file

---
+++ sample.js
@@ -0,0 +1,9 @@
+function printTableOf2() {
+  for (let i = 1; i <= 10; i++) {
+    const result = 2 * i;
+    console.log(`2 X ${i} = ${result}`);
+  }
+}
+
+// Call the function to print the table of 2
+printTableOf2();
