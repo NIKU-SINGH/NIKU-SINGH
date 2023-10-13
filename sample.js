62da1e348571c0dc23ffd70b6fe5e2c4391044ff
NIKU-SINGH <soapmactavishmw4@gmail.com>
2023-10-13 10:08:48 +0530

    added pascal traingle

--- sample.js
+++ sample.js
@@ -1,9 +1,29 @@
-function printTableOf2() {
-  for (let i = 1; i <= 10; i++) {
-    const result = 2 * i;
-    console.log(`2 X ${i} = ${result}`);
+function generatePascalsTriangle(row, col) {
+  if (col === 0 || col === row) {
+    return 1;
+  } else {
+    return generatePascalsTriangle(row - 1, col - 1) + generatePascalsTriangle(row - 1, col);
   }
 }
 
-// Call the function to print the table of 2
-printTableOf2();
+function printPascalsTriangle(rows) {
+  if (rows < 1) {
+    return;
+  }
+
+  const maxWidth = generatePascalsTriangle(rows - 1, Math.floor(rows / 2)).toString().length * 2;
+
+  for (let i = 0; i < rows; i++) {
+    let rowString = ' '.repeat((rows - i - 1) * maxWidth / 2);
+
+    for (let j = 0; j <= i; j++) {
+      const currentValue = generatePascalsTriangle(i, j);
+      rowString += currentValue.toString().padStart(maxWidth, ' ') + ' '.repeat(maxWidth);
+    }
+
+    console.log(rowString);
+  }
+}
+
+// Call the function to print Pascal's Triangle in pyramid shape with 5 rows.
+printPascalsTriangle(5);
